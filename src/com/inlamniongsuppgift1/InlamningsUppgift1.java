06758f2dd414a703a03e6b1a9992578924b75b8e
Lars Stromberg <stromberg.lars@gmail.com>
2018-09-12 11:58:03 +0200

    Skapat projetet och lagt in scannern

---
+++ src/com/inlamniongsuppgift1/InlamningsUppgift1.java
@@ -0,0 +1,10 @@
+package com.inlamniongsuppgift1;
+
+import java.util.Scanner;
+
+public class InlamningsUppgift1 {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
