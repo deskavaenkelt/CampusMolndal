1d15cb017425f1ddfe44ee8e9e2d98dab4e8a0c4
Lars Stromberg <stromberg.lars@gmail.com>
2018-09-15 09:37:47 +0200

    IU-2018.2.3 <83larstr@MS712866 Create Default.xml, debugger.xml, path.macros.xml

--- src/com/inlamniongsuppgift1/InlamningsUppgift1.java
+++ src/com/inlamniongsuppgift1/InlamningsUppgift1.java
@@ -5,23 +5,28 @@ import java.util.Scanner;
 public class InlamningsUppgift1 {
     public static void main(String[]args){
         Scanner scan = new Scanner(System.in);
-        // Programet avslutas inte förens det akivt väljs.
+        // (1) Programet avslutas inte förens det akivt väljs.
         String spela = "j";
         while (spela.equals("j")) {
 
+            // Bonus
             System.out.print("Skriv in ditt namn: ");
             String spelarNamn = scan.nextLine();
-
+            // (2) Antal kast
             System.out.print("Hej " + spelarNamn + " du kommer att spela mot Datorn. \nVälj antal kast som ska spelas: ");
             int antalKast = scan.nextInt();
 
-            // Varsin array för att lagra tärningskasten och summorna
-            int deltagare = 2;
-            int spelare[deltagare] = new int[antalKast];
-            int summaSpelare[deltagare];
+            // (3a) Ange hur många deltagare (G du & dator = 2).
+            int deltagare = 2;                      // Kommer ge spelare 0 & 1.
+            int spelare[] = new int[deltagare];     // Skapar array spelare bestående av "deltagare"-antal spelare.
 
-            // Anropa tärningen
-            tarning(antalKast, spelare, deltagare);
+            // (3b) Lagra resultatet i en tvådimensionell array "summa = rader\spelare = koloner".
+            int summa = 0;
+            int table[][] = new int[summa][spelare[deltagare]];
+
+
+            // Anropa tärningen, i en loop
+            spelare[deltagare] += tarning(antalKast);
 
             // Programmet avslutas om man svarar "nej" här.
             System.out.println("Vill du spela igen? (j/n) ");
@@ -29,13 +34,17 @@ public class InlamningsUppgift1 {
         }
         System.out.print("Tack och välkommen tillbaka!");
     }
-
-    // Skapar tärningskasten för de 2 spelarna
-    static void tarning(int inAntalKast, int inSpelare, int inDeltagare){
+    // (4) Skapar tärningskasten för spelarna, Printar dem och gör "return" på summan.
+    public static int tarning(int inAntalKast){
+        // int antalKast = inAntalKast;
         int antalSidor = 6;
+        int kastArray[] = new int[inAntalKast];
+        int summa = 0;
         for(int i=1 ; i <= inAntalKast ; i++){
-            int kastArray[i] = (int) (1+antalSidor*Math.random());
-
+            kastArray[i] = (int) (1+antalSidor*Math.random());
+            System.out.println("Kast "+ i + " gav: " + kastArray[i]);
+            summa += kastArray[i];
         }
+        return summa;
     }
 }
