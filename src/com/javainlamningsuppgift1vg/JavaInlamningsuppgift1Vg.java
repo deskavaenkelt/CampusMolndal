9636bc36a124bf65d10b66050a2e01edf90968ae
Lars Stromberg <stromberg.lars@gmail.com>
2018-09-15 15:13:00 +0200

    En första commit

---
+++ src/com/javainlamningsuppgift1vg/JavaInlamningsuppgift1Vg.java
@@ -0,0 +1,59 @@
+package com.javainlamningsuppgift1vg;
+import java.util.Scanner;
+public class JavaInlamningsuppgift1Vg {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        // (1) Programet avslutas inte förens det akivt väljs.
+        char spela = 'j';
+        // Bonus
+        System.out.print("Skriv in ditt namn: ");
+        String spelarNamn = scan.nextLine();
+        System.out.print("\nHej " + spelarNamn + " du kommer att spela mot Datorn. \n");
+
+        do{
+            // (2) Antal kast
+            System.out.print("\nVälj antal kast som ska spelas: ");
+            int antalKast = scan.nextInt();
+            System.out.print("\n");
+
+            // (3) Spelare skickar antalKast till tarning()
+            int summaSpelare = 0;
+            summaSpelare += tarning(antalKast, spelarNamn);
+
+            // (4) Dator skickar antalKast till tarning()
+            int summaDator = 0;
+            String datorNamn = "Dator";
+            summaDator += tarning(antalKast, datorNamn);
+
+            // Skriv ut summor mm i en färdig sträng
+            String textOut = spelarNamn + " fick totalt " + summaSpelare + " poäng. \nDatorn fick totalt " + summaDator + " poäng. \n";
+
+            // (5) Jämför resultaten och skriv ut vinnaren
+            if(summaSpelare == summaDator){
+                System.out.println(textOut + "\n" + "Det blev lika, ingen vann!\n");
+            } else if (summaSpelare > summaDator){
+                System.out.println(textOut + "\n" + spelarNamn + " vann!\n");
+            } else {
+                System.out.println(textOut + "\n" +"Datorn vann!\n");
+            }
+            System.out.println("Vill du spela igen? (j/n) ");
+            spela = scan.next().charAt(0);
+        } while(spela  != 'n');
+        // Program avslut!
+        System.out.print("\nTack och välkommen tillbaka!\n");
+    }
+    // (4) Skapar tärningskasten för spelarna, Printar dem och gör "return" på summan.
+    private static int tarning(int inAntalKast, String inSpelarNamn){
+        // int antalKast = inAntalKast;
+        int antalSidor = 6;
+        int summa = 0;
+        System.out.println(inSpelarNamn + ":");
+        for(int i=1 ; i <= inAntalKast ; i++){
+            int kast = (int) (1+antalSidor*Math.random());
+            System.out.println("Kast "+ i + " gav: " + kast);
+            summa += kast;
+        }
+        System.out.print("\n");
+        return summa;
+    }
+}
