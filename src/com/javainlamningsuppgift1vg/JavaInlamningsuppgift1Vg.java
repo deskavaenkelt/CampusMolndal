24da5fbe18a29f39c0a23cdaed0a294bd8736b7f
Lars Stromberg <stromberg.lars@gmail.com>
2018-09-17 23:08:01 +0200

    Börjat skriva programmet utifrån flödesdiagrammet

--- src/com/javainlamningsuppgift1vg/JavaInlamningsuppgift1Vg.java
+++ src/com/javainlamningsuppgift1vg/JavaInlamningsuppgift1Vg.java
@@ -3,7 +3,46 @@ import java.util.Scanner;
 public class JavaInlamningsuppgift1Vg {
     public static void main(String[]args){
         Scanner scan = new Scanner(System.in);
-        // (1) Programet avslutas inte förens det akivt väljs.
+        char spela = 'j';
+
+        System.out.print("Hur många spelare skall det vara? ");
+        int deltagare = scan.nextInt();
+
+        // Spelararrayen skapas
+        int spelare[] = new int[deltagare];                         // Används för att numrera spelare
+        String spelarNamn[] = new String[deltagare];                // Används för att döpa spelare
+
+        for(int i = 0 ; i <= deltagare ; i++){                      // Hur löser jag att string börjar på 0 och inte 1?
+            System.out.print("Namn på spelare " + i + ": ");        // Ger dubla utskrifter på första spelaren, varför?
+            spelarNamn[i] = scan .nextLine();
+        }
+
+        System.out.print("Hur många kast skall spelas? ");
+        int antalKast = scan.nextInt();
+
+        // Skicka spelare j till tärningen och spara summan i spelare[deltagare]
+        for(int j ; j <= deltagare ; j++){
+
+        }
+
+    }
+    // (4) Skapar tärningskasten för spelarna, Printar dem och gör "return" på summan.
+    private static int tarning(int inAntalKast, String inSpelarNamn){
+        // int antalKast = inAntalKast;
+        int antalSidor = 6;
+        int summa = 0;
+        System.out.println(inSpelarNamn + ":");
+        for(int i=1 ; i <= inAntalKast ; i++){
+            int kast = (int) (1+antalSidor*Math.random());
+            System.out.println("Kast "+ i + " gav: " + kast);
+            summa += kast;
+        }
+        System.out.print("\n");
+        return summa;
+    }
+}
+/*
+// (1) Programet avslutas inte förens det akivt väljs.
         char spela = 'j';
         // Bonus
         System.out.print("Skriv in ditt namn: ");
@@ -41,19 +80,4 @@ public class JavaInlamningsuppgift1Vg {
         } while(spela  != 'n');
         // Program avslut!
         System.out.print("\nTack och välkommen tillbaka!\n");
-    }
-    // (4) Skapar tärningskasten för spelarna, Printar dem och gör "return" på summan.
-    private static int tarning(int inAntalKast, String inSpelarNamn){
-        // int antalKast = inAntalKast;
-        int antalSidor = 6;
-        int summa = 0;
-        System.out.println(inSpelarNamn + ":");
-        for(int i=1 ; i <= inAntalKast ; i++){
-            int kast = (int) (1+antalSidor*Math.random());
-            System.out.println("Kast "+ i + " gav: " + kast);
-            summa += kast;
-        }
-        System.out.print("\n");
-        return summa;
-    }
-}
+ */
