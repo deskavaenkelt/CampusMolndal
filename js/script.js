1448407cecb5c780553d9d8939e5f27d275812e3
Lars Stromberg <stromberg.lars@gmail.com>
2018-12-05 21:05:56 +0100

    Påbörjat projektet igen efter sjukdom
    Mobilanpassning, korrigerat CSS och gjort koden enhetlig

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var userName = new Array();
+userName[0] = "Lars";
+var userPassword = new Array();
+userPassword[0] = "tjenare";
+
+var userNameEnterd = document.getElementById(userName).value;
+var userPasswordEnterd = document.getElementById(userPassword).value;
+
+if (var i= 0 ; i <= userName.length && userNameEnterd == userName[i]; i++){
+
+}
