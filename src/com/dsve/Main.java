a1fd364d8504076e83b597fdc34eeb1a3faababa
Lars Strömberg <stromberg.lars@gmail.com>
2019-02-08 15:46:52 +0100

    Bör var klart nu

--- src/com/dsve/Main.java
+++ src/com/dsve/Main.java
@@ -1,77 +1,41 @@
 package com.dsve;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static Scanner scan = new Scanner(System.in);
 
+    public static char[] charArray1 = { 's', 't', 'r', 'i', 'n', 'g' };
+    public static char[] charArray2 = { 's', 'i', 'n', 'g' };
+
     public static boolean globalFlag = true;
+    public static char[] charArrayE = new char[3];
+    public static ArrayList<String> stringArrayF = new ArrayList<>();
 
     public static void main(String[] args) {
-        /*char[] charArray1 = { 's', 't', 'r', 'i', 'n', 'g' };
-        char[] charArray2 = { 's', 'i', 'n', 'g' };
+        // TODO: Kör uppgift A till D
+        runAtoD();
 
-        // TODO: Startuppgift
-        System.out.println("Startuppgift");
-        char[] computedCharArray1 = detectTInArray(charArray1, 1);
-        char[] computedCharArray2 = detectTInArray(charArray2, 2);
-
-        // TODO: a)
-        // changAllRtoT
-        // in detectTInArray
-
-        // TODO: b)
-        // toUppercase(char[] charArray)
-        // in detectTInArray
-
-        // TODO: c)
-        // removeDoublets()
-        // in detectTInArray
-
-        // TODO: d)
-        // compareTwoArrays
-        compareTwoArrays(computedCharArray1, computedCharArray2);
-        compareTwoArrays(charArray1,charArray2);*/
-
-        // TODO: e)
-        //  Skapa input-fält
-        //inputField();
-
-        // TODO: f)
-        //  1) Skapa stringarray men en plats
-        //  2)
-        //  3)
-        //  4)
-        //  5)
-        //  6)
-        //  7)
-
-        // TODO: g)
-        //  1)
-        //  2)
-        //  3)
-        //  4)
-        //  5)
-        //  6)
-        //  7)
-
-        // TODO: h)
-        //  1)
-        //  2)
-        //  3)
-        //  4)
-        //  5)
-        //  6)
-        //  7)
-
-//        char[] temp = getValidCharArray(3);
-//        System.out.println("Returneras från getValidCharArray: " + Arrays.toString(temp));
+        // TODO: Kör uppgift E till G
         inputField();
+
         scan.close();
     }
 
+    private static void runAtoD() {
+        System.out.println("Uppgift A-D");
+        char[] computedCharArray1 = detectTInArray(charArray1, 1);
+        char[] computedCharArray2 = detectTInArray(charArray2, 2);
+        compareTwoArrays(computedCharArray1, computedCharArray2);
+        //compareTwoArrays(charArray1,charArray2);
+    }
+
+
+    // 0)
     private static char[] detectTInArray(char[] charArray, int id) {
         // TODO: om input innehåller t. Om sant skriv ut true på skärmen, annars false
         // TODO: ska kunna hanterqa andra arrays
@@ -84,20 +48,23 @@ public class Main {
                 returnValue = true;
             }
         }
-        System.out.println("charArray" + id + " i detectTInArray returnerar: " + returnValue);
+        System.out.println("\ncharArray" + id + " returnerar: " + returnValue);
+
+        System.out.println("0) " + Arrays.toString(charArray));
 
         changeAllRtoT(charArray);
-//        System.out.println(charArray);  // Debugg
 
+//        System.out.println(charArray);  // Debugg
         toUppercase(charArray);
 //        System.out.println(charArray);  // Debugg
 
         charArray = removeDoublets(charArray);
+        System.out.println("c) " + Arrays.toString(charArray));
 //        System.out.println(charArray);  // Debugg
 
         return charArray;
     }
-
+    // a)
     private static void changeAllRtoT(char[] charArray) {
         int i;
         for (i = 0; i < charArray.length; i++) {
@@ -105,8 +72,9 @@ public class Main {
                 charArray[i] = 't';
             }
         }
+        System.out.println("a) " + Arrays.toString(charArray));
     }
-
+    // b)
     private static void toUppercase(char[] charArray) {
         int i;
         for (i = 0; i < charArray.length; i++) {
@@ -114,8 +82,9 @@ public class Main {
                 charArray[i] = 'T';
             }
         }
+        System.out.println("b) " + Arrays.toString(charArray));
     }
-
+    // c)
     private static char[] removeDoublets(char[] charArray) {
         String onlyRemoveT = "";
 
@@ -127,7 +96,7 @@ public class Main {
         }
         return onlyRemoveT.toCharArray();
     }
-
+    // d)
     private static void compareTwoArrays(char[] charArray1, char[] charArray2) {
         boolean isIdentical = true;
         int i;
@@ -136,98 +105,89 @@ public class Main {
                 isIdentical = false;
             }
         }
+        System.out.println("\nd) \n" + Arrays.toString(charArray1) + "\n" + Arrays.toString(charArray2));
         if (isIdentical) {
-            System.out.println("Array 1 and Array 2 are identical");
+            System.out.println("Array 1 and Array 2 are identical\n");
         } /*else {
             System.out.println("Array 1 and Array 2 missmatched");  // Debugg
         }*/
     }
 
+
+
     private static void inputField() {
-        // TODO: e)
-        //  1) String tar emot vad som helst.   ==> userInputString()
-        //  2) Sätt in i en char array[3]       ==> getValidCharArray()
-        //  3) Om denna array är fylld skall programmet fråga om ny input               ej null mm, måste få plats i char
-        //  4) I denna input skall en siffra skrivas in för vilken plats i er array som skall ersättas med det nya värdet.
-        //  5) Programmet skall därefter sätta in det nya värdet på begärd plats och skriva ut er array.
-        //  6) När detta är gjort skall programmet begära ny input om input är tom skall programmet brytas.
-        //  7) Tom input bryter programmet.
+        System.out.println("Uppgift E-G)");
 
-        char[] aCharArray = getValidCharArray(3);
-        System.out.println("aCharArray: " + Arrays.toString(aCharArray));                // debug
-
-        globalFlag = true;
         while (globalFlag) {
-            globalFlag = false;
-
-            int replaceCharAtPos = userInputReadNumbers(0, 2);
-            System.out.println("replaceCharAtPos: " + replaceCharAtPos);                     // debug
-
-            char[] replaceCharWith = getValidCharArray(1);
-            System.out.println("replaceCharWith: " + Arrays.toString(replaceCharWith));      // debug
+            System.out.print("Skriv in en char: ");
+            String temp = getStringInput(false);
+            if (!globalFlag) {
+                break;
+            }
+            //System.out.println("stringArrayF innehåller: " + Arrays.toString(stringArrayF.toArray()));  // debug
+            //System.out.println("Returnerat värde: " + temp.charAt(0)); //debug
+            int placeHolder = getIntInput(0, 2);
+            if (!globalFlag) {
+                break;
+            }
+            charArrayE[placeHolder] = temp.charAt(0);
         }
 
+        System.out.println("\ne)\n" + Arrays.toString(charArrayE));                     // e)
+
+        System.out.println(toFormattedString(charArrayE));
+        
+        System.out.println("\nf)\n" + Arrays.toString(stringArrayF.toArray()));         // f)
 
     }
-
-    private static char[] getValidCharArray(int maxLengtOfReturnedCharArray) {
-
-        char[] inputFieldCharArray = new char[maxLengtOfReturnedCharArray];
-        boolean flag = true;
-
-        int i;
-        int counter = 0;
-        while (flag) {
-            flag = false;
-            String recivedString = userInputString();
-            char[] tempCharArray = recivedString.toCharArray();
-
-            for (i = 0; i < tempCharArray.length; i++) {
-                inputFieldCharArray[counter] = tempCharArray[i];
-                counter++;
-                if (counter == 3) {
-                    break;
-                }
-            }
-            if (counter < maxLengtOfReturnedCharArray) {
-                flag = true;
-            }
-        }
-        return inputFieldCharArray;
-    }   // Kontrollerad och fungerar
-
-    private static String userInputString() {
-        globalFlag = true;
-        System.out.print("Skriv in en sträng: ");
+    private static String getStringInput(boolean isNumber) {
         String userInput = scan.nextLine();
-        if (userInput.isEmpty()) {
+        int length = userInput.length();
+        if (length == 0) {
             globalFlag = false;
+            return "*";
+        }
+        if (!isNumber) {
+            addToStringArray(userInput);    // g)
         }
         return userInput;
-    }           // kontrollerad och fungerar, returnerar hel stäng utan vidare kontroll
-
-    private static int userInputReadNumbers(int min, int max) {
-        globalFlag =true;
+    }                        // g)
+    private static int getIntInput(int min, int max) {
         System.out.print("Skriv in ett tal mellan " + min + "-" + max + ": ");
-        int scannedInt = 0;
-        boolean dontBreakLoop = true;
+        int userInput = -1;
 
-        int temp = scan.hasNextInt();
+        boolean flag = true;
+        while (flag) {
+            flag = false;
 
-        while (dontBreakLoop) {
-            boolean hasNextInt = scan.hasNextInt();
-            if (hasNextInt) {
-                scannedInt = scan.nextInt();
-                scan.nextLine(); // handle next line character (enter key)
-                if (scannedInt >= min && scannedInt <= max) {
-                    dontBreakLoop = false;
-                } else if (scannedInt == ' ') {
-                    globalFlag = false;
+            String temp = getStringInput(true);
+            if (temp.charAt(0) == '0' || temp.charAt(0) == '1' || temp.charAt(0) == '2') {
+                userInput = Integer.parseInt(temp);
+
+                if (userInput >= min && userInput <= max) {
+                    return userInput;
                 } else {
                     System.out.println("Mata in giltlig siffra");
+                    flag = true;
                 }
+
+            } else {
+                globalFlag = false;
+                return -1;
             }
         }
-        return scannedInt;
-    }    // kontrollerad och fungerar
+        return userInput;
+    }
+    private static void addToStringArray(String input) {
+        stringArrayF.add(input);
+    }
+    public static String toFormattedString(char[] a)
+    {
+        if (a.length==1) return String.valueOf(a[0]);
+        else
+            return a[0]+toFormattedString(Arrays.copyOfRange(a,1,a.length)) ;
+
+    }
 }
+
+
