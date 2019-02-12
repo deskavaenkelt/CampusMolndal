f34fc03a9615c5fdb591f60e9439ad24c514b685
Lars Strömberg <stromberg.lars@gmail.com>
2019-02-12 21:33:05 +0100

    lagt in funktion för att kolla primtal

--- src/com/dsve/Main.java
+++ src/com/dsve/Main.java
@@ -1,8 +1,32 @@
 package com.dsve;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+	    long number = 17L;
+
+        System.out.println("Är inmatat numer ett primtal? " + isPrime(number));
+
+    }
+
+    public static boolean isPrime(long number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (long i = 2; i < Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
+
+
+/*
+* 88. Bug Fix for ArrayList Challenge innhåller mycket som löser uppgiften
+* */
