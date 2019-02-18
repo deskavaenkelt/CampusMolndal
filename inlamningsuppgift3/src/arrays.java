a21174ab603875871c5830c9fe7db62e8f2e8e6d
Mattan1999 <mattias470@gmail.com>
2019-02-18 13:54:13 +0100

    Update arrays.java
    La till Getters och Setters.

--- inlamningsuppgift3/src/arrays.java
+++ inlamningsuppgift3/src/arrays.java
@@ -9,6 +9,53 @@ public class arrays {
     private static ArrayList<Integer> evenNumbers = new ArrayList<>();
     private static ArrayList<Integer> oddNumbers = new ArrayList<>();
 
-    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 
+    public static ArrayList<String> getStrings() {
+        return strings;
+    }
+
+    public static ArrayList<Integer> getFibonacci() {
+        return fibonacci;
+    }
+
+    public static ArrayList<Date> getTimeStamp() {
+        return timeStamp;
+    }
+
+    public static ArrayList<Integer> getEvenNumbers() {
+        return evenNumbers;
+    }
+
+    public static ArrayList<Integer> getOddNumbers() {
+        return oddNumbers;
+    }
+
+    public SimpleDateFormat getDateFormat() {
+        return dateFormat;
+    }
+
+    public static void setStrings(ArrayList<String> strings) {
+        arrays.strings = strings;
+    }
+
+    public static void setFibonacci(ArrayList<Integer> fibonacci) {
+        arrays.fibonacci = fibonacci;
+    }
+
+    public static void setTimeStamp(ArrayList<Date> timeStamp) {
+        arrays.timeStamp = timeStamp;
+    }
+
+    public static void setEvenNumbers(ArrayList<Integer> evenNumbers) {
+        arrays.evenNumbers = evenNumbers;
+    }
+
+    public static void setOddNumbers(ArrayList<Integer> oddNumbers) {
+        arrays.oddNumbers = oddNumbers;
+    }
+
+    public void setDateFormat(SimpleDateFormat dateFormat) {
+        this.dateFormat = dateFormat;
+    }
 }
