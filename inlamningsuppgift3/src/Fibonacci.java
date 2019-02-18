b332cc44a8a9411aaccfd11907bf863fb39e6dc2
Lars <stromberg.lars@gmail.com>
2019-02-18 23:08:41 +0100

    Update Fibonacci.java
    Borde fungera, ej testad

--- inlamningsuppgift3/src/Fibonacci.java
+++ inlamningsuppgift3/src/Fibonacci.java
@@ -2,4 +2,20 @@ public class Fibonacci {
 
     //https://www.java-examples.com/fibonacci-series-java-example
 
+    private static int counter = 0;
+
+    /** Call this function to add a new fibonacci number to the series */
+    protected void addNewFibonacci() {
+        createAnewFibonacciElement();
+    }
+
+    private static void createAnewFibonacciElement() {
+        if (0 == counter || 1 == counter) {
+            Arrays.setFibonacci(counter);
+            counter++;
+        } else {
+            Arrays.setFibonacci(Arrays.getFibonacci().get(counter-1) + Arrays.getFibonacci().get(counter-2));
+            counter++;
+        }
+    }
 }
