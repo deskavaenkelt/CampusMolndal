61a17dc0bdc747808b8e59325d7690679bad9072
[madde1] <[maddeswish@gmail.com]>
2019-02-19 10:36:57 +0100

    Fixat så det även tar bort i timestamp

--- inlamningsuppgift3/src/Delete.java
+++ inlamningsuppgift3/src/Delete.java
@@ -1,17 +1,15 @@
 /**
  * datalogiUppgift3slutuppgift
  *
- * @author Lars Strömberg
+ * @author Lars Strömberg , Madeleine Hallqvist
  * @version 1.0
  * @since 2019-02-18
  * https://github.com/deskavaenkelt/
  */
 public class Delete {
-    protected static void start() {
-    deleteValueInArray();
-    }
 
-    private static void deleteValueInArray() {
+
+    protected static void deleteValueInArray() {
         System.out.println("Skriv in vad vill du radera: ");
         String checkValue = UserInput.getString();
         int index = Arrays.getStrings().indexOf(checkValue);
@@ -21,14 +19,16 @@ public class Delete {
             if (element.equals(checkValue)) {
                 found = true;
                 Arrays.getStrings().remove(index);
-                //Arrays.getTimeStamp().remove(index);   ta bort kommentars // när getTimeStamp får in värden.
+                Arrays.getTimeStamp().remove(index);
                 System.out.println(checkValue + " har tagits bort");
+
             }
             if(!found){
                 System.out.println("Det du skrivit in finns ej");
                 break;
             }
             System.out.println(Arrays.getStrings());
+            System.out.println(Arrays.getTimeStamp());
 
         }
     }
