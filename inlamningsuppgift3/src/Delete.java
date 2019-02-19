8311488168091492bb53a34729b060cc0df30fe2
[madde1] <[maddeswish@gmail.com]>
2019-02-19 13:15:27 +0100

    Update Delete.java

--- inlamningsuppgift3/src/Delete.java
+++ inlamningsuppgift3/src/Delete.java
@@ -7,7 +7,28 @@
  * https://github.com/deskavaenkelt/
  */
 public class Delete {
-
-
     
+    public class Delete {
+
+        protected static void deleteStart(){
+            deleteValueInArray();
+        }
+
+
+        private static void deleteValueInArray() {
+            System.out.println("Skriv in vad vill du radera: ");
+            String checkValue = UserInput.getString();
+            int index = Search.serachAndGetStringIndex(checkValue);
+
+            if (Arrays.getStrings().contains(checkValue)) {
+                System.out.println(checkValue + " Är bort tagen");
+                Arrays.getStrings().remove(index);
+                Arrays.getTimeStamp().remove(index);
+
+            } else {
+                System.out.println(checkValue + "kunde inte hittas");
+            }
+
+        }
+    }
 }
