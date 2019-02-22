d42467aa264a30879f79c01d11bf982f18cc192e
Lars Strömberg <stromberg.lars@gmail.com>
2019-02-22 13:33:03 +0100

    code optimize

--- inlamningsuppgift3/src/Arrays.java
+++ inlamningsuppgift3/src/Arrays.java
@@ -18,7 +18,6 @@ class Arrays {
     private static ArrayList<Integer> evenNumbers = new ArrayList<>();
     private static ArrayList<Integer> oddNumbers = new ArrayList<>();
 
-    //https://www.quora.com/How-do-I-sort-an-ArrayList-of-dates-based-on-the-current-date-in-Java
     private SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 
     //----- Getters -----//
