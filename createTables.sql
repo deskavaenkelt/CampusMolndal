4c8bac25b8cfb54b8c7888eae4ef8536e050b52f
Lars Strömberg <stromberg.lars@gmail.com>
2019-03-12 14:37:23 +0100

    Create createTables.sql
    Skapat databas plus 7 tabeller

---
+++ createTables.sql
@@ -0,0 +1,75 @@
+CREATE DATABASE dsve_cinema_booking_system;
+USE dsve_cinema_booking_system;
+
+-- 1
+CREATE TABLE films
+(
+  filmsId           INT PRIMARY KEY AUTO_INCREMENT,
+  filmName          VARCHAR(45) NOT NULL UNIQUE,
+  filmLengthMinutes INT         NOT NULL
+);
+
+
+-- 2
+CREATE TABLE customers
+(
+  customerId        INT PRIMARY KEY AUTO_INCREMENT,
+  customerFirstName VARCHAR(45),
+  customerLastName  VARCHAR(45),
+  customerGender    ENUM ('M', 'F') NOT NULL,
+  customerEmail     VARCHAR(45)     NOT NULL UNIQUE
+);
+
+
+-- 3
+CREATE TABLE rooms
+(
+  roomId            INT PRIMARY KEY AUTO_INCREMENT,
+  roomName          VARCHAR(45) NOT NULL,
+  roomNumberOfSeats INT         NOT NULL
+);
+
+
+-- 4
+CREATE TABLE screenings
+(
+  screeningId        INT PRIMARY KEY AUTO_INCREMENT,
+  screeningStartTime DATETIME NOT NULL,
+  screeningFilmId    INT      NOT NULL,
+  screeningRoomId    INT      NOT NULL,
+  FOREIGN KEY (screeningFilmId) REFERENCES films (filmsId),
+  FOREIGN KEY (screeningRoomId) REFERENCES rooms (roomId)
+);
+
+
+-- 5
+CREATE TABLE seats
+(
+  seatsId    INT PRIMARY KEY AUTO_INCREMENT,
+  seatRow    CHAR(1) NOT NULL,
+  seatNumber INT     NOT NULL,
+  seatRoomId INT     NOT NULL,
+  FOREIGN KEY (seatRoomId) REFERENCES rooms (roomId)
+);
+
+
+-- 6
+CREATE TABLE bookings
+(
+  bookingId          INT PRIMARY KEY AUTO_INCREMENT,
+  bookingScreeningId INT NOT NULL,
+  bookingCustomerId  INT NOT NULL,
+  FOREIGN KEY (bookingScreeningId) REFERENCES screenings (screeningId),
+  FOREIGN KEY (bookingCustomerId) REFERENCES customers (customerId)
+);
+
+
+-- 7
+CREATE TABLE reservedSeats
+(
+  reservedSeatId        INT PRIMARY KEY NOT NULL,
+  reservedSeatBookingId INT             NOT NULL,
+  reservedSeatSeatId    INT             NOT NULL,
+  FOREIGN KEY (reservedSeatBookingId) REFERENCES bookings (bookingId),
+  FOREIGN KEY (reservedSeatSeatId) REFERENCES seats (seatsId)
+);
