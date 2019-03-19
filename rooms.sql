d7e427ae96f8c2a56815f234d4c9261b79a79e63
Lars Strömberg <stromberg.lars@gmail.com>
2019-03-19 09:58:56 +0100

    Create rooms.sql

---
+++ rooms.sql
@@ -0,0 +1,30 @@
+/*
+  __
+ /\ \
+ \_\ \    ____  __  __     __
+ /'_` \  /',__\/\ \/\ \  /'__`\
+/\ \L\ \/\__, `\ \ \_/ |/\  __/
+\ \___,_\/\____/\ \___/ \ \____\
+ \/__,_ /\/___/  \/__/   \/____/
+
+*/
+
+USE dsve_cinema_booking_system;
+
+SHOW TABLES;
+
+DESCRIBE rooms;
+
+SELECT * FROM rooms;
+
+
+INSERT INTO rooms (roomName, roomNumberOfSeats)
+VALUES
+       ('Stark',72),
+       ('Rogers',36),
+       ('Banner',18);
+
+
+SELECT *
+FROM rooms
+ORDER BY roomName;
